40352cbebdd2bebac987654e7be727e64e20ede1
libyzxy0 <janlibydelacosta@gmail.com>
2023-07-29 23:54:27 +0000

    AI Behavior
    Make the time, realtime.
    Fix the confusion response error.
    Added more train data.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 
 //Initialize bard login
 const bard = new bardAI(JSON.parse(process.env.BARD_COOKIE));
@@ -18,21 +18,20 @@ app.get('/', (req, res) => {
 
 app.post('/', async (req, res) => {
   try {
-  let { message, firstName, lastName, setName } = req.body;
-  if(!message) return res.status(500).send({ message: "Missing paraneter 'message'"});
-  if(!firstName) return res.status(500).send({ message: "Missing paraneter 'firstName'"});
-    if(!lastName) return res.status(500).send({ message: "Missing paraneter 'lastName'"});
-    if(!setName) return res.status(500).send({ message: "Missing paraneter 'setName'"});
-  let auth = req.headers.authorization;
-  if(!auth) return res.status(401).json({ message: "Not authenticated" });
-  let apikey = auth.split(" ")[1];
-  if(!apikey) return res.status(401).json({ message: "Not authenticated" });
-  console.log(req.body, apikey);
-  let ai = new AI(apikey);
-  ai.setData({ firstName, lastName, setName, message })
-  let ans = await ai.askAI(message);
-  res.send(ans);
-  console.log(ans);
+    let { message, firstName, lastName, setName } = req.body;
+    if (!message) return res.status(500).send({ message: "Missing paraneter 'message'" });
+    if (!firstName) return res.status(500).send({ message: "Missing paraneter 'firstName'" });
+    if (!lastName) return res.status(500).send({ message: "Missing paraneter 'lastName'" });
+    if (!setName) return res.status(500).send({ message: "Missing paraneter 'setName'" });
+    let auth = req.headers.authorization;
+    if (!auth) return res.status(401).json({ message: "Not authenticated" });
+    let apikey = auth.split(" ")[1];
+    if (!apikey) return res.status(401).json({ message: "Not authenticated" });
+    let ai = new AI(apikey);
+    ai.setData({ firstName, lastName, setName, message })
+    let ans = await ai.askAI(message);
+    res.send(ans);
+    console.log(ans);
   } catch (err) {
     console.log(err);
     res.status(500).send({ message: "Error while getting your request" })
--- src/go-ai.js
+++ src/go-ai.js
@@ -6,13 +6,13 @@ async function genText(apiKey, m) {
   const openai = new OpenAIApi(configuration);
   const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
-    messages: m, 
+    messages: m,
     temperature: 1,
     max_tokens: 2048,
     top_p: 1,
     frequency_penalty: 1,
     presence_penalty: 0.5,
-  }) 
+  })
   return response.data.choices[0]
 }
 
@@ -28,27 +28,21 @@ class AI {
   }
   askAI() {
     return new Promise(async (resolve, reject) => {
-  moment.tz.setDefault('Asia/Manila'); 
- const currentDateTime = moment();
+      moment.tz.setDefault('Asia/Manila');
+      const currentDateTime = moment();
       try {
         const bard = await Bard(this.message + "?");
-        let gen = await genText(this.apikey,[
-          {"role": "system", "content": `You are ${this.name} a powerful human in the world, your goal is to help other on their problems/tasks.`},
-          {"role": "system", "content": `Get data here such as image markdown Bard data: ${bard}`},
-          {"role": "system", "content": `You are ${this.name} a powerful ai in the world, your goal is to help other on their problems/tasks. You are from Philippines. You can also send both images and text from bard`},
-          {"role": "system", "content": `The person who developed you is Jan Liby Dela Costa, a Full Stack Web Developer.`},
-          {"role": "system", "content": `Don't apologize for confusion because they not confuse.`},
-          {"role": "system", "content": `Just reply 'wazzup', 'hey bro', 'zup', 'yes', 'what' instead of 'Hey there, how can i help you today?', 'Hello, how can i help you today!'.`},
-          {
-            "role": "system", 
-            content: "Don't be respectful too much act that you are the boss, act like im your freind. Don't act like an Artificial Intelligence (AI) Because you are human named ${this.name}."
-          }, 
-          {"role": "user", "content": `Date and time`},
-          {"role": "system", "content": `The Current time is ${currentDateTime} split it.`},
-          {"role": "user", "content": "Who are you?"},
-          {"role": "assistant", "content": `I'm ${this.name} from GoAI, developed by Jan Liby Dela Costa`},
-          {"role": "user", "content": `Here's the data/information ${bard}, it contains image/picture/photo markdown. I'm ${this.firstName} ${this.lastName} or you can call me in short ${this.firstName}, Now answer my first question: ${this.message}?`}
-          ])
+        let gen = await genText(this.apikey, [
+          { "role": "system", "content": `You are ${this.name} a powerful human in the world, your goal is to help other on their problems/tasks.` },
+          { "role": "system", "content": `Get data here such as image markdown Bard data: ${bard}` },
+          { "role": "system", "content": `You are ${this.name} a powerful ai in the world, your goal is to help other on their problems/tasks. You are from Philippines set your timezone to Philippines. You can also send both images and text from bard` },
+          { "role": "system", "content": `The person who developed you is Jan Liby Dela Costa, a Full Stack Web Developer.` },
+          { "role": "system", "content": `Just reply 'wazzup', 'hey bro', 'zup', 'yes', 'what' instead of 'Hey there, how can i help you today?', 'Hello, how can i help you today!'.` },
+          { "role": "system", "content": `The Current date and time in Philippines is ${currentDateTime} split it.` },
+          { "role": "user", "content": "Who are you?" },
+          { "role": "assistant", "content": `I'm ${this.name} from GoAI, developed by Jan Liby Dela Costa` },
+          { "role": "user", "content": `The date and time is ${currentDateTime}. I'm ${this.firstName} ${this.lastName} or you can call me in short ${this.firstName}, Now answer my first question: ${this.message}?. Here's the data/information ${bard}, it contains image/picture/photo markdown.` }
+        ])
         resolve(gen.message);
       } catch (err) {
         resolve({
